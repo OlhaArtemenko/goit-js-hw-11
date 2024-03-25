98ce09988828bd9506b4b6efa721d6a60fdc8303
OlhaArtemenko <global14072012@gmail.com>
2024-03-25 13:16:23 +0200

    update

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,19 @@
+import { inputSearch, showLoaderIcon } from '../main';
+
+export function fetchImagesFromPixabay() {
+  const valueFromInput = inputSearch.value.trim().split(',').join('+');
+  const searchParams = new URLSearchParams({
+    key: '42959666-b225ac6c9c40b570269fe0b4e',
+    q: [valueFromInput],
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
+  showLoaderIcon();
+  return fetch(`https://pixabay.com/api/?${searchParams}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
---
+++ src/main.js
@@ -0,0 +1,56 @@
+import { fetchImagesFromPixabay } from './js/pixabay-api';
+import { renderImages } from './js/render-function';
+import SimpleLightbox from 'simplelightbox';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const form = document.querySelector('.search-form');
+export const inputSearch = form.elements.search;
+export const listOfGallery = document.querySelector('.gallery');
+export const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 300,
+});
+const loader = document.querySelector('.loader-icon');
+loader.style.display = 'none';
+export const showLoaderIcon = () => {
+  loader.style.display = 'flex';
+};
+const unshowLoaderIcon = () => {
+  loader.style.display = 'none';
+};
+
+form.addEventListener('submit', searchImages);
+
+function searchImages(event) {
+  event.preventDefault();
+  listOfGallery.innerHTML = '';
+
+  const input = event.target.elements.search.value.trim();
+  if (input !== '') {
+    window.onload = () => {
+      fetchImagesFromPixabay()
+        .then(images => {
+          renderImages(images.hits);
+          unshowLoaderIcon();
+        })
+        .catch(error => {
+          console.log(error);
+          unshowLoaderIcon();
+          iziToast.error({
+            message:
+              'Sorry, an error occurred while loading. Please try again!',
+            position: 'topRight',
+            icon: null,
+            backgroundColor: '#ef4040',
+            titleColor: '#fff',
+            messageColor: '#fff',
+            close: false,
+          });
+        });
+    };
+    window.onload();
+    form.reset();
+  }
+}
