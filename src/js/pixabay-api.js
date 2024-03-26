4e821dafe3458a536c89ac204283ca9cfc7801c7
OlhaArtemenko <global14072012@gmail.com>
2024-03-26 10:00:05 +0200

    mistakes changes

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,18 +1,15 @@
-import { inputSearch, showLoaderIcon } from '../main';
-
-export function fetchImagesFromPixabay() {
-  const valueFromInput = inputSearch.value.trim().split(',').join('+');
+export function fetchImagesFromPixabay(query) {
   const searchParams = new URLSearchParams({
     key: '42959666-b225ac6c9c40b570269fe0b4e',
-    q: [valueFromInput],
+    q: query.split(',').join('+'),
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
   });
-  showLoaderIcon();
+
   return fetch(`https://pixabay.com/api/?${searchParams}`).then(response => {
     if (!response.ok) {
-      throw new Error(response.status);
+      throw new Error(response.statusText);
     }
     return response.json();
   });
--- src/main.js
+++ src/main.js
@@ -6,18 +6,20 @@ import 'izitoast/dist/css/iziToast.min.css';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const form = document.querySelector('.search-form');
-export const inputSearch = form.elements.search;
-export const listOfGallery = document.querySelector('.gallery');
-export const lightbox = new SimpleLightbox('.gallery a', {
+const inputSearch = form.elements.search;
+const listOfGallery = document.querySelector('.gallery');
+const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 300,
 });
 const loader = document.querySelector('.loader');
 loader.style.display = 'none';
-export const showLoaderIcon = () => {
+
+const showLoaderIcon = () => {
   loader.style.display = 'flex';
 };
-const unshowLoaderIcon = () => {
+
+const hideLoaderIcon = () => {
   loader.style.display = 'none';
 };
 
@@ -28,28 +30,34 @@ function searchImages(event) {
   listOfGallery.innerHTML = '';
 
   const input = event.target.elements.search.value.trim();
-  if (input !== '') {
-    window.onload = () => {
-      fetchImagesFromPixabay()
-        .then(images => {
-          renderImages(images.hits);
-          unshowLoaderIcon();
-        })
-        .catch(error => {
-          console.log(error);
-          unshowLoaderIcon();
-          iziToast.error({
-            message:
-              'Sorry, an error occurred while loading. Please try again!',
-            position: 'topRight',
-            icon: null,
-            backgroundColor: '#ef4040',
-            titleColor: '#fff',
-            messageColor: '#fff',
-          });
+  if (input.length >= 3) {
+    showLoaderIcon();
+    fetchImagesFromPixabay(input)
+      .then(images => {
+        renderImages(images.hits);
+        hideLoaderIcon();
+      })
+      .catch(error => {
+        console.error(error);
+        hideLoaderIcon();
+        iziToast.error({
+          message: 'Sorry, an error occurred while loading. Please try again!',
+          position: 'topRight',
+          icon: null,
+          backgroundColor: '#ef4040',
+          titleColor: '#fff',
+          messageColor: '#fff',
         });
-    };
-    window.onload();
+      });
     form.reset();
+  } else {
+    iziToast.warning({
+      message: 'Please enter at least 3 characters for search',
+      position: 'topRight',
+      icon: null,
+      backgroundColor: '#ffc107',
+      titleColor: '#fff',
+      messageColor: '#fff',
+    });
   }
 }
